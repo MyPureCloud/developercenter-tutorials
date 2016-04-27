40dc9a765514a5411b92925986ac2f2b3d9dc1d4
Smith, Tim <Tim.Smith@inin.com>
2016-04-27 16:03:38 -0600

    [API-1480] Use mypurecloud

--- oauth-implicit/ruby/script.rb
+++ oauth-implicit/ruby/script.rb
@@ -7,7 +7,7 @@ set :port, 8085
 #this method will be called once we get our authentication token
 def post_auth_method authToken
     #just get the current user's information and print it out to the console
-    userInfo = RestClient.get('https://api.inindca.com/api/v1/users/me', {:Authorization => 'Bearer ' + authToken})
+    userInfo = RestClient.get('https://api.mypurecloud.com/api/v1/users/me', {:Authorization => 'Bearer ' + authToken})
 
     puts JSON.pretty_generate (JSON.parse(userInfo))
 
@@ -30,7 +30,7 @@ Thread.new {
   sleep(1)
   client_id = 'bfadf7a0-3364-4f65-9fda-00d37877113f';
 
-  redirectUri = "https://login.inindca.com/oauth/authorize?" +
+  redirectUri = "https://login.mypurecloud.com/oauth/authorize?" +
               "response_type=token" +
               "&client_id=" + client_id +
               "&redirect_uri=http://localhost:8085/oauth2/callback";
