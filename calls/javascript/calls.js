9bbd8999bb494bfeed5288f814e328aad3bb644e
Tim Smith <tim.j.smith@genesys.com>
2018-05-31 09:49:18 -0600

    API-2665 update calls JS tutorial

--- calls/javascript/calls.js
+++ calls/javascript/calls.js
@@ -1,161 +1,170 @@
-var CONVERSATION_LIST_TEMPLATE = null;
+/* globals Handlebars */
 
+// This client ID expects the redirect URL to be http://localhost:8080/
+const clientId = 'babbc081-0761-4f16-8f56-071aa402ebcb';
+const redirectUri = window.location.href;
+
+// Set purecloud objects
+const platformClient = require('platformClient');
+const client = platformClient.ApiClient.instance;
+const conversationsApi = new platformClient.ConversationsApi();
+
+// Set PureCloud settings
+client.setEnvironment('mypurecloud.com');
+client.setPersistSettings(true, 'test_app');
+
+// Set local vars
+let CONVERSATION_LIST_TEMPLATE = null;
+let interval;
+let conversationList = [];
+
+// Set up handlebars
 Handlebars.registerHelper('unlessCond', function(v1, v2, options) {
-  if(v1 !== v2) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
+	if(v1 !== v2) {
+		return options.fn(this);
+	}
+	return options.inverse(this);
 });
 
 Handlebars.registerHelper('isConsult', function(participants, options) {
-  if(participants.length > 2) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
+	if(participants.length > 2) {
+		return options.fn(this);
+	}
+	return options.inverse(this);
 });
-
-var clientId = '6cf4b2d8-26ed-4f44-af81-932a8d8b9404';
-var redirectUri = 'http://localhost:8080/index.html';
-
-var pureCloudSession = new purecloud.platform.PureCloudSession({
-    strategy: 'implicit',
-    clientId: clientId,
-    redirectUrl: redirectUri,
-    environment: 'mypurecloud.com'
-});
-pureCloudSession.login();
-
-var conversationsApi = new purecloud.platform.ConversationsApi(pureCloudSession);
-var interval;
-var conversationList = [];
 
 // jQuery calls this when the DOM is available
 $(document).ready(function() {
-    CONVERSATION_LIST_TEMPLATE =Handlebars.compile($("#entry-template").html());
+	client.loginImplicitGrant(clientId, redirectUri)
+		.then(() => {
+			console.log('Logged in');
+			CONVERSATION_LIST_TEMPLATE = Handlebars.compile($('#entry-template').html());
 
-    // Handle the toggle polling button click
-    $('button#togglePolling').click(function() {
-        if (interval) {
-            clearInterval(interval);
-            interval = null;
-            $('button#togglePolling').text('Start Queue Polling');
-        } else {
-            interval = setInterval(checkQueue, 1000);
-            $('button#togglePolling').text('Stop Queue Polling');
-        }
-    });
+			// Handle the toggle polling button click
+			$('button#togglePolling').click(function() {
+				if (interval) {
+					clearInterval(interval);
+					interval = null;
+					$('button#togglePolling').text('Start Queue Polling');
+				} else {
+					interval = setInterval(checkQueue, 1000);
+					$('button#togglePolling').text('Stop Queue Polling');
+				}
+			});
 
-    // Handle dial button click
-    $('button#dial').click(function() {
-        // Create request body
-        var body = {
-            "phoneNumber":$('input#dialstring').val()
-        };
+			// Handle dial button click
+			$('button#dial').click(function() {
+				// Create request body
+				let body = {
+					'phoneNumber':$('input#dialstring').val()
+				};
 
-        // Invoke API
-        conversationsApi.postCalls(body).then(function(result) {
-            // Clear dialstring from text box
-            $('input#dialstring').val('');
-        }).catch(function(err){
-            console.error(err);
-        });
-    });
+				// Invoke API
+				conversationsApi.postConversationsCalls(body).then(function(result) {
+					// Clear dialstring from text box
+					$('input#dialstring').val('');
+				}).catch(function(err){
+					console.error(err);
+				});
+			});
+		})
+		.catch((err) => console.error(err));
 });
 
 // Gets current queue contents and displays it
 function checkQueue() {
-    console.debug("Checking queue...");
-    // Invoke API
-    conversationsApi.getCalls().then(function(calls) {
-        conversationList = calls.entities;
-        var html = CONVERSATION_LIST_TEMPLATE(calls.entities);
-        $('#call-table').html(html);
-    });
+	console.debug('Checking queue...');
+	// Invoke API
+	conversationsApi.getConversationsCalls().then(function(calls) {
+		conversationList = calls.entities;
+		let html = CONVERSATION_LIST_TEMPLATE(calls.entities);
+		$('#call-table').html(html);
+	});
 }
 
 // Mute participant
 function mute(callId, participantId, currentMuteState) {
-    // Create request body
-    var body = {
-        "muted": !currentMuteState
-    };
+	// Create request body
+	let body = {
+		'muted': !currentMuteState
+	};
 
-    // Invoke API
-    conversationsApi.patchCallsCallIdParticipantsParticipantId(callId, participantId, body).then(function(result) {
-        // Result will be empty here
-    }).catch(function(err){
-        console.error(err);
-    });
+	// Invoke API
+	conversationsApi.patchConversationsCallParticipant(callId, participantId, body).then(function(result) {
+		// Result will be empty here
+	}).catch(function(err){
+		console.error(err);
+	});
 }
 
 // Hold participant
 function hold(callId, participantId, currentHoldState) {
-    // Create request body
-    var body = {
-        "held": !currentHoldState
-    };
+	// Create request body
+	let body = {
+		'held': !currentHoldState
+	};
 
-    // Invoke API
-    conversationsApi.patchCallsCallIdParticipantsParticipantId(callId, participantId, body).then(function(result) {
-        // Result will be empty here
-    }).catch(function(err){
-        console.error(err);
-    });
+	// Invoke API
+	conversationsApi.patchConversationsCallParticipant(callId, participantId, body).then(function(result) {
+		// Result will be empty here
+	}).catch(function(err){
+		console.error(err);
+	});
 }
 
 // Disconnect participant
 function disconnect(callId, participantId) {
-    // Create request body
-    var body = {
-        "state": "disconnected"
-    };
+	// Create request body
+	let body = {
+		'state': 'disconnected'
+	};
 
-    // Invoke API
-    conversationsApi.patchCallsCallIdParticipantsParticipantId(callId, participantId, body).then(function(result) {
-        // Result will be empty here
-    }).catch(function(err){
-        console.error(err);
-    });
+	// Invoke API
+	conversationsApi.patchConversationsCallParticipant(callId, participantId, body).then(function(result) {
+		// Result will be empty here
+	}).catch(function(err){
+		console.error(err);
+	});
 }
 
 function startConsult() {
-    var callId = conversationList[0].id;
+	let callId = conversationList[0].id;
 
-    //grab the first participant which is going to be the party we dialed for an outbound call
-    var participantId = conversationList[0].participants[1].id;
+	//grab the first participant which is going to be the party we dialed for an outbound call
+	let participantId = conversationList[0].participants[1].id;
 
-    // Create request body
-    var body = {
-        "speakTo": "destination",
-        "destination":{
-            "address" : $('input#newparticipant').val()
-        }
-    };
+	// Create request body
+	let body = {
+		'speakTo': 'destination',
+		'destination':{
+			'address' : $('input#newparticipant').val()
+		}
+	};
 
-    // Invoke API
-    conversationsApi.postCallsCallIdParticipantsParticipantIdConsult(callId, participantId, body).then(function(result) {
-        $('input#newparticipant').val('');
-        // We can ignore the response in this tutorial.
-    }).catch(function(err){
-        console.error(err);
-    });
+	// Invoke API
+	conversationsApi.patchConversationsCallParticipantConsult(callId, participantId, body).then(function(result) {
+		$('input#newparticipant').val('');
+		// We can ignore the response in this tutorial.
+	}).catch(function(err){
+		console.error(err);
+	});
 }
 
 function consultSpeakTo(speakTo) {
-    var callId = conversationList[0].id;
+	let callId = conversationList[0].id;
 
-    //grab the first participant which is going to be the party we dialed for an outbound call
-    var participantId = conversationList[0].participants[1].id;
+	//grab the first participant which is going to be the party we dialed for an outbound call
+	let participantId = conversationList[0].participants[1].id;
 
-    // Create request body
-    var body = {
-        "speakTo": speakTo
-    };
+	// Create request body
+	let body = {
+		'speakTo': speakTo
+	};
 
-    // Invoke API
-    conversationsApi.patchCallsCallIdParticipantsParticipantIdConsult(callId, participantId, body).then(function(result) {
-        // We can ignore the response in this tutorial.
-    }).catch(function(err){
-        console.error(err);
-    });
+	// Invoke API
+	conversationsApi.patchConversationsCallParticipantConsult(callId, participantId, body).then(function(result) {
+		// We can ignore the response in this tutorial.
+	}).catch(function(err){
+		console.error(err);
+	});
 }
