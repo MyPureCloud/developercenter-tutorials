8f9b1b57b188b44c5f59f83be63aaa33172347c6
deerghayu <deerghayu.shrestha@gmail.com>
2019-10-09 18:41:16 -0400

    API-4785 update create-campaign with auth helper

--- create-campaign/python/create-campaign.py
+++ create-campaign/python/create-campaign.py
@@ -1,4 +1,4 @@
-import base64, csv, sys, requests
+import base64, csv, sys, requests, os
 import PureCloudPlatformClientV2
 from pprint import pprint
 from PureCloudPlatformClientV2.rest import ApiException
@@ -7,37 +7,13 @@ print("-------------------------------------------------------------")
 print("- Python3 Create an Outbound Dialing Campaign -")
 print("-------------------------------------------------------------")
 
+# Get Authorization Token
+apiClient = PureCloudPlatformClientV2.api_client.ApiClient().get_client_credentials_token(os.environ['PURECLOUD_CLIENT_ID'], os.environ['PURECLOUD_CLIENT_SECRET'])
+
 # PureCloud Objects
-outbound_api = PureCloudPlatformClientV2.OutboundApi()
-scripts_api = PureCloudPlatformClientV2.ScriptsApi()
-routing_api = PureCloudPlatformClientV2.RoutingApi()
-
-# OAuth when using Client Credentials
-client_id = "CLIENT_ID"
-client_secret = "CLIENT_SECRET"
-authorization = base64.b64encode(bytes(client_id + ":" + client_secret, "ISO-8859-1")).decode("ascii")
-
-# Prepare for POST /oauth/token request
-request_headers = {
-    "Authorization": f"Basic {authorization}",
-    "Content-Type": "application/x-www-form-urlencoded"
-}
-request_body = {
-    "grant_type": "client_credentials"
-}
-
-# Get token
-response = requests.post("https://login.mypurecloud.com/oauth/token", data=request_body, headers=request_headers)
-
-# Check response
-if response.status_code == 200:
-    print("Got token")
-else:
-    print(f"Failure: { str(response.status_code) } - { response.reason }")
-    sys.exit(response.status_code)
-
-# Assign the token
-PureCloudPlatformClientV2.configuration.access_token = response.json()["access_token"]
+outbound_api = PureCloudPlatformClientV2.OutboundApi(apiClient)
+scripts_api = PureCloudPlatformClientV2.ScriptsApi(apiClient)
+routing_api = PureCloudPlatformClientV2.RoutingApi(apiClient)
 
 # Set your own values here
 CONTACT_LIST_NAME = "A List"
@@ -85,15 +61,15 @@ campaign_configuration = PureCloudPlatformClientV2.Campaign()
 
 campaign_configuration.name = "My Campaign 1"
 campaign_configuration.dialing_mode = "preview"
-campaign_configuration.contact_list = PureCloudPlatformClientV2.UriReference()
+campaign_configuration.contact_list = PureCloudPlatformClientV2.DomainEntityRef()
 campaign_configuration.contact_list.id = contact_list_id
 campaign_configuration.phone_columns = [PureCloudPlatformClientV2.PhoneColumn()]
 campaign_configuration.phone_columns[0].column_name = "phone"
 campaign_configuration.phone_columns[0].type = "home"
 pprint(campaign_configuration.phone_columns)
-campaign_configuration.queue = PureCloudPlatformClientV2.UriReference()
+campaign_configuration.queue = PureCloudPlatformClientV2.DomainEntityRef()
 campaign_configuration.queue.id = queue_id
-campaign_configuration.script = PureCloudPlatformClientV2.UriReference()
+campaign_configuration.script = PureCloudPlatformClientV2.DomainEntityRef()
 campaign_configuration.script.id = script_id
 campaign_configuration.caller_name = "Caller ID"
 campaign_configuration.caller_address = "5551231234"
