2fd95e9e878d5b22cb0f1d56f3c939fc09efb0c0
hero2bash <zero.bash@gmail.com>
2020-06-04 19:24:18 +0800

    Add Python version of Agentless SMS Notifications

---
+++ agentless-sms-notifications/python/agentless-sms-notifications.py
@@ -0,0 +1,29 @@
+import os
+import PureCloudPlatformClientV2
+from pprint import pprint
+from PureCloudPlatformClientV2.rest import ApiException
+
+print("-------------------------------------------------------------")
+print("- Agentless SMS Notifications -")
+print("-------------------------------------------------------------")
+
+# OAuth when using Client Credentials
+apiClient = PureCloudPlatformClientV2.api_client.ApiClient() \
+            .get_client_credentials_token(os.environ['PURECLOUD_CLIENT_ID'],
+                                          os.environ['PURECLOUD_CLIENT_SECRET'])
+
+# PureCloud Objects
+conversations_api = PureCloudPlatformClientV2.ConversationsApi(apiClient)
+
+# Body of the request
+body = PureCloudPlatformClientV2.SendAgentlessOutboundMessageRequest()
+body.from_address = "+13178723000"
+body.to_address = "+15557655942"
+body.to_address_messenger_type = "sms"
+body.text_body = "Hello, this is a test notification"
+
+try:
+    response = conversations_api.post_conversations_messages_agentless(body)
+    pprint(response)
+except ApiException as e:
+    print("Exception when calling ConversationsApi->post_conversations_messages_agentless: %s\n" % e)
