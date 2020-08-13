ca43ca3173813452e40105c4d6103b8713766a2c
Drake Rozenboom <drake.rozenboom@genesys.com>
2020-08-13 11:18:17 -0400

    Changes client ID and secret to Genesys_Cloud

--- historical-adherence-query/java/Main.java
+++ historical-adherence-query/java/Main.java
@@ -32,8 +32,8 @@ public class Main {
         TimeoutException {
 
         // Set client credentials
-        String clientId = System.getenv("PURECLOUD_CLIENT_ID");
-        String clientSecret = System.getenv("PURECLOUD_CLIENT_SECRET");
+        String clientId = System.getenv("GENESYS_CLOUD_CLIENT_ID");
+        String clientSecret = System.getenv("GENESYS_CLOUD_CLIENT_SECRET");
 
         // Set Region
         PureCloudRegionHosts region = PureCloudRegionHosts.us_east_1;
