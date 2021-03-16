4b8d72374bdaf3b934a58a6f7dd7006564e892f2
hero2bash <zero.bash@gmail.com>
2021-03-16 15:49:30 +0800

    DEVENGAG-548: Fix upload-prompts (nodejs)

--- upload-prompts/nodejs/index.js
+++ upload-prompts/nodejs/index.js
@@ -1,6 +1,7 @@
 const platformClient = require('purecloud-platform-client-v2');
 const fs = require('fs');
-const request = require('request-promise');
+const axios = require('axios');   
+const FormData = require('form-data');
 
 // Get client credentials from environment variables
 const GENESYS_CLOUD_CLIENT_ID = process.env.GENESYS_CLOUD_CLIENT_ID;
@@ -21,10 +22,8 @@ client.loginClientCredentialsGrant(GENESYS_CLOUD_CLIENT_ID, GENESYS_CLOUD_CLIENT
 		// Create new prompt
 		console.log('Creating new prompt...');
 		return architectApi.postArchitectPrompts({ 
-			body: {
 				name: 'uploaded_prompt',
 				description: 'Prompt uploaded by upload-prompts example app'
-			}
 		});
 	})
 	.then((prompt) => {
@@ -32,31 +31,27 @@ client.loginClientCredentialsGrant(GENESYS_CLOUD_CLIENT_ID, GENESYS_CLOUD_CLIENT
 
 		// Create prompt resource for english
 		console.log('Creating prompt resource...');
-		return architectApi.postArchitectPromptResources(prompt, { 
-			body: {
+		return architectApi.postArchitectPromptResources(prompt.id, { 
 				language: 'en-us'
-			}
 		});
 	})
 	.then((promptResource) => {
 		console.log(promptResource);
-
-		// Upload WAV file to prompt
 		console.log('Uploading prompt...');
-		return request({
-			method: 'POST',
-			uri: promptResource.uploadUri,
-			formData: {
-				file: fs.createReadStream('../prompt-example.wav')
-			},
+
+		const form = new FormData();
+		form.append('file', fs.createReadStream('../prompt-example.wav'));
+
+		return axios.post(promptResource.uploadUri, form, {
 			headers: {
-				Authorization: 'bearer ' + client.authData.accessToken
+				'Authorization': 'bearer ' + client.authData.accessToken,
+				...form.getHeaders()
 			}
 		});
 	})
 	.then((res) => {
 		console.log('Upload complete. Review your prompt in architect.');
 		console.log('https://apps.mypurecloud.com/architect/#/call/userprompts');
-		console.log(res);
+		console.log(res.data);
 	})
 	.catch((err) => console.log(err));
