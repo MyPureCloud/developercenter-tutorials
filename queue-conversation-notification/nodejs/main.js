9ded892bbf19ab7c5f0cbbd2671c26947d06dab0
hero2bash <zero.bash@gmail.com>
2021-03-19 13:15:16 +0800

    DEVENGAGE-543: Remake queue-conversation

---
+++ queue-conversation-notification/nodejs/main.js
@@ -0,0 +1,89 @@
+const platformClient = require('purecloud-platform-client-v2');
+var WebSocketClient = require('websocket').client;
+
+// Genesys Cloud Credentials
+const GENESYS_CLOUD_CLIENT_ID = process.env.GENESYS_CLOUD_CLIENT_ID;
+const GENESYS_CLOUD_CLIENT_SECRET = process.env.GENESYS_CLOUD_CLIENT_SECRET;
+const GENESYS_CLOUD_ENVIRONMENT = 'mypurecloud.com';
+
+const client = platformClient.ApiClient.instance;
+const websocketClient = new WebSocketClient();
+
+// API instances
+const notificationsApi = new platformClient.NotificationsApi();
+
+// Additional configuration variables
+const queueId = '--- QUEUE ID HERE ---';
+const subscriptionTopic = `v2.routing.queues.${queueId}.conversations`;
+
+
+// Define the callbacks for the websocket listener
+websocketClient.on('connect', connection => {
+    console.log('WebSocket client connected and listening...');
+    connection.on('message', message => {
+        let data = JSON.parse(message.utf8Data);
+        let topic = data.topicName;
+        let eventBody = data.eventBody;
+
+        if(topic == subscriptionTopic){
+            let conversation = eventBody;
+            let customer = conversation.participants
+                            .filter(p => p.purpose == 'customer')[0];
+            let agent = conversation.participants
+                            .filter(p => p.purpose == 'agent')[0];
+
+            // Some messages based on events that are happening on the queue
+            if(customer && !agent){
+                console.log(`Customer ${customer.id} is waiting on queue.`);
+            }
+            if(agent && !agent.connectedTime){
+                console.log(`Agent ${agent.name} is being alerted for customer ${customer.id}`);
+            }
+            if(agent && agent.connectedTime){
+                console.log(`Agent ${agent.name} has accepted conversation with ${customer.id}`);
+            }
+            if(customer.endTime){
+                console.log(`Customer ${customer.id} disconnected from conversation`);
+            }
+        }
+
+        // For heartbeats
+        if(topic == 'channel.metadata'){
+            console.log(eventBody.message);
+        }
+    });
+});
+
+websocketClient.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+
+
+// Authenticate with Genesys Cloud
+client.setEnvironment(GENESYS_CLOUD_ENVIRONMENT);
+client.loginClientCredentialsGrant(GENESYS_CLOUD_CLIENT_ID, GENESYS_CLOUD_CLIENT_SECRET)
+.then(() => {
+    console.log('Authentication successful');
+
+    return notificationsApi.postNotificationsChannels()
+})
+.then(data => {
+    console.log('Channel created');
+
+    let websocketUri = data.connectUri;
+    let channelId = data.id;
+    websocketClient.connect(websocketUri);
+
+    // Add the subscription for queue events
+    let topic = [{id: subscriptionTopic}];
+    return notificationsApi.postNotificationsChannelSubscriptions(channelId, topic);
+})
+.then(data => {
+    console.log('Subscribed to Queue');
+})
+.catch(e => console.error(e));
+
+
+// NOTE: This next line is a simple way to make sure the app runs 'forever'
+// In production you'd want the actual code running as a process/service
+setInterval(() => {}, 1 << 30);
